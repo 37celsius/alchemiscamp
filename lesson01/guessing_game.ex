42a5784f17c37e4878938b524c63139956b6a44e
Beta Ngadiman <eltheo@Eltheo-MBP.local>
2019-12-08 09:28:53 +1100

    lesson 1

---
+++ lesson01/guessing_game.ex
@@ -0,0 +1,30 @@
+defmodule GuessingGame do
+  def guess(a, b) when a > b, do: guess(b, a)
+
+  def guess(low, high) do
+    user_input = IO.gets "Hmmm.. are you thinking of the number #{mid(low, high)}?\n"
+
+    case String.trim(user_input) do
+      "bigger" -> bigger(low, high)
+      "smaller" -> smaller(low, high)
+      "yes" -> "Ha! I knew it!"
+      _ -> IO.puts ~s(Type "bigger", "smaller" or "yes")
+            guesss(low, high)
+    end
+
+  end
+
+  def bigger(low, high) do
+    new_low = min high, mid(low, high) + 1
+    guess(new_low, high)
+  end
+
+  def smaller(low, high) do
+    new_high = max low, mid(low, high) - 1
+    guess(low, new_high)
+  end
+
+  def mid(a, b) do
+    div (a + b), 2
+  end
+end
